0c4289f6beba9bdc6711e5f0cd28cc6708cafb47
hardwood6341 <asadkufa@gmail.com>
2025-01-29 19:56:26 +0300

    first commit

---
+++ Projectn1 channal-name.py
@@ -0,0 +1,6 @@
+print ("Welcom to the YouTube Channel Name Generator:")
+nick_name = input ("What is your nickname? \n")
+Job = input ("What is your channel about? \n") 
+print ("You could name your channel " + "(" + Job + " with " + nick_name + ")")
+input ("do you want to know how many letters are in your nick name? \n" )
+print (len (nick_name)) 
