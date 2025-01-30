23050288ef73c25386a17bfa6584318630611a2e
hardwood6341 <asadkufa@gmail.com>
2025-01-30 23:34:08 +0300

    update project 3

--- project 3 count the time .py
+++ project 3 count the time .py
@@ -1,3 +1,4 @@
+#first method
 total_second = int (input ("Please enter the number of seconds:\n"))
 h = total_second //3600
 remining_second = total_second % 3600
@@ -6,3 +7,10 @@ s = remining_second % 60
 print ("The total time of the course is" ,h,"hours and" ,m,"minuts and" ,s,"seconds")
 
 #الغايه من هذا معرفه ال // و % 
+
+#second method
+total_seconds = int(input("please type the number of seconds : \n"))
+hours = total_seconds // 3600
+minuts = ((total_seconds) - (hours*3600)) // 60
+seconds = total_seconds % 60
+print("total time is " +  str(hours) + " hours " + str(minuts) + " minuts " + str(seconds) + " seconds ")
