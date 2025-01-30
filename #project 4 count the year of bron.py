1dde207750e3c188df5f52a52094ebd82eb327f6
hardwood6341 <asadkufa@gmail.com>
2025-01-31 01:13:52 +0300

    update project 4

--- #project 4 count the year of bron.py
+++ #project 4 count the year of bron.py
@@ -1,3 +1,8 @@
 #project 4 count the year of bron
 x = int (input ("How old are you? \n"))
 print ("You Were born in the year", 2025 - x)
+
+
+# second method / by the f-string 
+# x = int (input ("How old are you? \n"))
+# print (f"You Were born in the year {2025 - x}")
