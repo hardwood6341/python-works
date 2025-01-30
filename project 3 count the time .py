7c60e8cfde5446aa521069c348e55fe45edf709d
hardwood6341 <asadkufa@gmail.com>
2025-01-30 23:10:28 +0300

    update project 3

---
+++ project 3 count the time .py
@@ -0,0 +1,8 @@
+total_second = int (input ("Please enter the number of seconds:\n"))
+h = total_second //3600
+remining_second = total_second % 3600
+m= remining_second // 60 
+s = remining_second % 60
+print ("The total time of the course is" ,h,"hours and" ,m,"minuts and" ,s,"seconds")
+
+#الغايه من هذا معرفه ال / و % 
