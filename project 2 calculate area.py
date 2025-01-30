a77ebcd6e8c0cd24da268bd2db3acc08f423a31c
hardwood6341 <asadkufa@gmail.com>
2025-01-30 17:40:48 +0300

    update project 2

--- project 2 calculate area.py
+++ project 2 calculate area.py
@@ -1,3 +1,4 @@
+#first method
 Length = input ("Please type Length : \n")
 Width = input ("Please type Width : \n")
 Price = input (" how much for 1 meter? \n")
@@ -6,3 +7,11 @@ print (" Give the guy :",((float (Length)* float (Width)) * (float (Price)) ))
 
 # هنا الشيء المهم انه يمكن في حالة وضع فارزه ان نجعل 
 #print يطبع اكثر من نوع داتا
+
+#second method
+x = input (" what is the length? \n")
+y = input (" what is the width? \n")
+total_area =  float (x)  * float (y)
+print ("total area is : ", (total_area))
+z = float (input (" what is the price for 1 meter, you want to pay? \n"))
+print (" Give the guy:", total_area * z ,"$" )
