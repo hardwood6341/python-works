ec88c3564fa4d6d10de4b3eb5cc82c5e9b9c8604
hardwood6341 <asadkufa@gmail.com>
2025-01-31 00:26:05 +0300

    change at project 2 & 3

--- project 3 count the time .py
+++ project 3 count the time .py
@@ -8,16 +8,16 @@ print ("The total time of the course is" ,h,"hours and" ,m,"minuts and" ,s,"seco
 
 #الغايه من هذا معرفه ال // و % 
 
-#second method
-total_seconds = int(input("please type the number of seconds : \n"))
-hours = total_seconds // 3600
-minuts = ((total_seconds) - (hours*3600)) // 60
-seconds = total_seconds % 60
-print("total time is " +  str(hours) + " hours " + str(minuts) + " minuts " + str(seconds) + " seconds ")
+# #second method
+# total_seconds = int(input("please type the number of seconds : \n"))
+# hours = total_seconds // 3600
+# minuts = ((total_seconds) - (hours*3600)) // 60
+# seconds = total_seconds % 60
+# print("total time is " +  str(hours) + " hours " + str(minuts) + " minuts " + str(seconds) + " seconds ")
 
-#third method
-total_second = int (input ("Please enter the number of seconds:\n"))
-h = int (total_second / 3600)
-m= int (((total_second) - ( h *3600)) / 60) 
-s = (((total_second) - ( h *3600))- (m * 60))
-print ("The total time of the course is" ,h,"hours and" ,m,"minuts and" ,s,"seconds")
+# #third method
+# total_second = int (input ("Please enter the number of seconds:\n"))
+# h = int (total_second / 3600)
+# m= int (((total_second) - ( h *3600)) / 60) 
+# s = (((total_second) - ( h *3600))- (m * 60))
+# print ("The total time of the course is" ,h,"hours and" ,m,"minuts and" ,s,"seconds")
