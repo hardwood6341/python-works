e07fa96f3d42f2e20f8e33ef85b618bdc64973b7
hardwood6341 <asadkufa@gmail.com>
2025-01-30 17:17:38 +0300

    update project 2

--- project 2 calculate area.py
+++ project 2 calculate area.py
@@ -3,3 +3,6 @@ Width = input ("Please type Width : \n")
 Price = input (" how much for 1 meter? \n")
 print (" total erae is :" , (float (Length) * float (Width)))
 print (" Give the guy :",((float (Length)* float (Width)) * (float (Price)) ))
+
+# هنا الشيء المهم انه يمكن في حالة وضع فارزه ان نجعل 
+#print يطبع اكثر من نوع داتا
