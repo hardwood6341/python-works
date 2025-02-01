e530b437b756a025bda08942e817f6492fa06d14
hardwood6341 <asadkufa@gmail.com>
2025-02-01 22:46:22 +0300

    create new project number 7

---
+++ #project 7 use AND & OR.py
@@ -0,0 +1,11 @@
+#project 7 use AND & OR 
+ 
+age = int (input ("Please, Enter your age?  \n"))
+licensec= input ("Please, Do you have a licensec? (yes or no)  \n")
+if age > 18 and licensec.lower() == "yes" :
+   print ("you can drive") 
+else : 
+  print ("sorry, you can't drive")
+
+#تم استعمال lower  ❤️
+# ومعرفة فائدة or , and والفرق بينهم ❤️
