79fff278d6c69c8dcb8fde8a8303a2b0606b18fd
hardwood6341 <asadkufa@gmail.com>
2025-02-03 23:14:19 +0300

    update project 14

--- #project 14 (datebase) list.py
+++ #project 14 (datebase) list.py
@@ -1,9 +1,22 @@
 #project 14 (datebase) list
 
-
+#project 1
 close_friend = ["saif", "akram", "ahmed", "ali"]
 print (f" the first name on our list is {close_friend[0]} and the last name on our list is {close_friend[-1]} ")
 
 close_friend[0] = "saiufy"
 
 print (close_friend)
+
+#project 2
+# colors = []
+# a = input ("Add the first color you like : \n").lower()
+# colors.append(a) 
+# b = input ("Do you want to add more colors? Yes or No?  \n").lower()
+# if b == "yes" :
+#   c = input ("Add another color to the list: \n").lower()
+#   colors.append(c)
+#   print (f"the colors you like are {colors}")
+# else:
+#   print (f"The color you like is {colors}")
+
