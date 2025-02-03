78c857135d273682fb6b2de13804a8f4beef5298
hardwood6341 <asadkufa@gmail.com>
2025-02-03 21:56:53 +0300

    update

---
+++ #project 14 (datebase) list.py
@@ -0,0 +1,9 @@
+#project 14 (datebase) list
+
+
+close_friend = ["saif", "akram", "ahmed", "ali"]
+print (f" the first name on our list is {close_friend[0]} and the last name on our list is {close_friend[-1]} ")
+
+close_friend[0] = "saiufy"
+
+print (close_friend)
