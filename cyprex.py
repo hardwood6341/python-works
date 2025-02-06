8ac477879608edc6a38d7be353818d36d0313903
hardwood6341 <asadkufa@gmail.com>
2025-02-06 23:09:04 +0300

    update

--- cyprex.py
+++ cyprex.py
@@ -1,3 +1,2 @@
-
-print ("friut = [['apples','bananas'], ['milk', 'water']]")
-print ([['oranges', 'apples','Bananas','kiwis'],['coffee','milk','tea'],[1,2,3]])
+fruits = [['apples','Bananas','kiwis'],['coffee','milk','tea'] ]
+print (fruits)
