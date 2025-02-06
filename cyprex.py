8ddfece850c876b8736bc1f0c3d12a13b92209e2
hardwood6341 <asadkufa@gmail.com>
2025-02-06 22:15:54 +0300

    update

--- cyprex.py
+++ cyprex.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-fruits = [['apple', 'banana', 'cherry'],  ['milk', 'bread']]
-fruits[0].insert(0, 'orange')
-print (fruits)
-=======
+
 print ("friut = [['apples','bananas'], ['milk', 'water']]")
-x = input ("Press enter to change the content ....")
-if x == "" :
-    print ([['oranges', 'apples','Bananas','kiwis'],['coffee','milk','tea'],[1,2,3]])
->>>>>>> b45c0b0153b62571037b26e203ab28157502ec1b
+print ([['oranges', 'apples','Bananas','kiwis'],['coffee','milk','tea'],[1,2,3]])
