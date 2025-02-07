fd0f3525a0ea4198ca054d268dbf7770da6bbd7c
hardwood6341 <asadkufa@gmail.com>
2025-02-07 10:10:11 +0300

    update

---
+++ cyprex.py
@@ -0,0 +1,9 @@
+print ("Welcome to the Rock, Paper, Scissors game:")
+a = input ("Press Enter to contiune or type (Help) for the rules help:").lower()
+if a :
+    print ("""
+           ******** RULES ********
+           1) You chiise abd tge computer chooses
+           2) Rock smashes Scissors -> Roxk wins 
+           3) Scissors cut paper -> Scissors win 
+           4) Paper covers Rock -> Paper wins""")
