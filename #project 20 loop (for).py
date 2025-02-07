1dceb9fc562bbfbce2360388c128ff27cde93510
hardwood6341 <asadkufa@gmail.com>
2025-02-07 22:32:42 +0300

    update every thing

--- #project 20 loop (for).py
+++ #project 20 loop (for).py
@@ -1,4 +1,5 @@
 #project 20 loop (for)
+#subproject 1
 ASCII = {2:"""
 ██████╗░
 ╚════██╗
@@ -45,3 +46,41 @@ for x in numbers:
      if x > 8 : 
          print (ASCII[x])
 print ("the loop greated than 8 numbers is finshed seccsifly")
+
+#subproject 2
+# attendees = ["ali", "mouhamed", "ibrahim"]
+# for person in attendees:
+#     print (person)
+#     a = input ("is this person attending? (yes/no):").lower()
+#     if a != 'yes':
+#         print ("Attendance not confirmed")
+        
+#     else:
+#      print ("attendance confirmed")
+#     print ("-------")
+
+#subproject 3
+# a = input ("Enter the names of attendees separated by commas:").lower()
+# b = a.split(", ")
+# for y in b:
+#    print ("\n",y,"\n")
+#    c = input ("is this person attending? (Yes or no)").lower()
+#    if c == 'yes':
+#       print ("attendance conformed")
+#    else:
+#       print ("attendance not conformed")
+#    print ("________")  
+
+#    #subproject 4
+# good = []
+# not_good = []
+# a = input("Enter your tasks for today separated by commas: ").split(", ")
+# for y in a:
+#     c = input(f"Did you finish {y} already?").lower()
+#     if c == 'yes':
+#         good.append(y)
+#         print("Nice job!")
+#     else:
+#         not_good.append(y)
+#         print("Try not to put it down")
+# print(f"***** Done tasks ***** {good}")
