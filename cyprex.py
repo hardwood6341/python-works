f025b8f916411388f056bc3c846e0414420b29aa
hardwood6341 <asadkufa@gmail.com>
2025-02-11 00:32:06 +0300

    update

--- cyprex.py
+++ cyprex.py
@@ -1,9 +1,18 @@
+# input: ali ahmed, yasser alwan, mouhamed hasan
 a = input("Enter the first and last name of your friends separated by comma:").split(", ")
 # output ['ali ahmed', 'yasser alwan', 'mouhamed hasan']
-result = []
 for name in a:
-   a= name.split(" ")
-   a= name.capitalize()
-   result.extend(a)
-# output ['ali', 'ahmed', 'yasser', 'alwan', 'mouhamed', 'hasan']
-print (result)
+    x = name.split(" ")
+    # output ['ali', 'ahmed']
+    #  ['yasser', 'alwan']
+t applying     #  ['mouhamed', 'hasa']
+    s = [] 
+    b = []
+    for part in x:
+        s.append(part.capitalize())
+    print(s)
+
+# output ['
+#ali', 'ahmed', 'yasser', 'alwan', 'mouhamed', 'hasan']
+# print (result)
+# # ali ahmed, yasser alwan, mouhamed hasa
