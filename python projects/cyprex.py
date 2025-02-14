6e302c9a930832c56cdcbde05c1d069d99324800
hardwood6341 <asadkufa@gmail.com>
2025-02-14 15:54:01 +0300

    update the json

--- python projects/cyprex.py
+++ python projects/cyprex.py
@@ -1,15 +1,5 @@
-# input: ali ahmed, yasser alwan, mouhamed hasan
-names = input (" write the names sapareted by comama :").split(", ")
-#output ["ali ahmes", "yasser alwan", "mouhamed hasan"]
-for x in names:
-  single_names=  x.split(" ")
-#['ali', 'ahmed']
-# ['yasser', 'alwan']
-# ['mouhamed', 'hasan']
-  result = []
-  for a in single_names:
-     print (a)
-     capitalize_names = a.capitalize()
-     result.append(capitalize_names)
-  print (result)
-  
+#* input: how are you
+s = input ("Enter a sentence : ").split(" ")
+#? output: ["how","are","you"]
+print (s[-1: :-1]) 
+#? 
